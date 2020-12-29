04c47a562fa30b0f8f865d687fd8395fceca090f
Abhishek Pratapa <abhishekpratapa@gmail.com>
2020-12-29 01:24:03 -0600

    Config file

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,17 @@
 import ptrading
 
-def test_init():
-	config = ptrading.config.Config
+def test_config_init():
+	time_unit = ptrading.utils.TimeUnit.ONE_MINUTE
+	tickers = ["AAPL", "MSFT"]
+	pre_market = False
+	start_date = datetime.datetime(2020, 3, 18)
+	end_date = datetime.datetime(2020, 4, 20)
+
+	config = ptrading.config.Config(tickers, pre_market, time_unit, start_date, end_date)
+	assert True
+
+def test_user_config_init():
+	principal = 50000
+
+	user_config = ptrading.config.UserConfig(principal)
 	assert True
